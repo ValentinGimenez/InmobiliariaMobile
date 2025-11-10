83c7f6676414df964f8c6cf9d9f0b461fc0725b5
Valentin Alejandro Gimenez <valentingimenez1909@gmail.com>
2025-11-10 13:20:22 -0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.android.material:material:1.10.0")
 }
